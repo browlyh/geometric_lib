68d4ded2ceff0ef96adea102601b0a65a2341679
Browly <qquusseeyynn@gmail.com>
2024-11-13 21:56:32 +0300

    added descreption for functions square.py and circle.py

--- circle.py
+++ circle.py
@@ -2,9 +2,11 @@ import math
 
 
 def area(r):
+    ''' Возвращает площадь окружности с радиусом = r '''
     return math.pi * r * r
 
 
 def perimeter(r):
+    ''' Возвращает длину окружности с радиусом = r '''
     return 2 * math.pi * r
 
